0600640957f0c26627ea29961b9bbedf7b175cea
marcinchaluda <chaluda@interia.pl>
2020-03-16 14:37:25 +0100

    Modify introduction in gittie file

--- gittie.py
+++ gittie.py
@@ -1,5 +1,5 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Hello, I'm not working properly!")
 
 def joke():
     print("What's the name of bear whitout a teeth? ..... A gummybear")
